ee32693dc97aa6b2790f13321e9637fcc0c3aea4
17372 <ak.kavali007@gmail.com>
2024-12-23 18:34:52 -0600

    initial commit

---
+++ src/main/java/com/example/git_execution_sample/GitExecutionSampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.git_execution_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExecutionSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExecutionSampleApplication.class, args);
+		System.out.println("Hello World Git Sample Execution!");
+		System.out.println("print statement added");
+	}
+
+}
